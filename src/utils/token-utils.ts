28d8e1769012ad1b23487135303cbd147c058e0c
不如去早睡 <582181384@qq.com>
2022-08-26 15:42:47 +0800

    更新注释

--- src/utils/token-utils.ts
+++ src/utils/token-utils.ts
@@ -1,13 +1,14 @@
+// 定义token字符串
 const TokenKey = 'vue_admin_template_token'
-
+// 从本地存储中获取token
 export function getToken() {
   return localStorage.getItem(TokenKey)
 }
-
+// 从本地存储中设置token
 export function setToken(token: string) {
   return localStorage.setItem(TokenKey, token)
 }
-
+// 从本地存储中删除token
 export function removeToken() {
   return localStorage.removeItem(TokenKey)
 }
