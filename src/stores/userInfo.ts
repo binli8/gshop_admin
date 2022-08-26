e8af0b5eb2cee34906bd3be94dc957c0f075f8a5
不如去早睡 <582181384@qq.com>
2022-08-26 19:41:16 +0800

    前天储存token

--- src/stores/userInfo.ts
+++ src/stores/userInfo.ts
@@ -3,6 +3,9 @@ import { getToken, removeToken, setToken } from '../utils/token-utils';
 import type { UserInfoState } from './interface';
 import {ElMessage} from 'element-plus'
 import {staticRoutes} from '@/router/routes'
+// 引入登录相关的接口(登录接口函数,获取用户信息的接口函数,退出的接口函数)
+import {loginApi,infoApi,logoutApi} from '@/api/login'
+
 
 
 /**
@@ -10,7 +13,7 @@ import {staticRoutes} from '@/router/routes'
  * @methods setUserInfos 设置用户信息
  */
 export const useUserInfoStore = defineStore('userInfo', {
-
+  // 状态数据
 	state: (): UserInfoState => ({
     token: getToken() as string,
     name: '',
@@ -19,34 +22,30 @@ export const useUserInfoStore = defineStore('userInfo', {
   }),
 
 	actions: {
-    login (username: string, password: string) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (username==='admin' && password==='111111') {
-            const token = 'token-atguigu'
-            setToken(token)
-            this.token = token
-            resolve(token)
-          } else {
-            reject(new Error('用户名或密码错误!'))
-            ElMessage.error('用户名或密码错误!')
-          }
-        }, 1000)
-      })
+    // 登录的方法,传入用户名和密码
+    async login (username: string, password: string) {
+    // 登录,传入用户名和密码
+    const {token} = await loginApi(username,password)
+    // 储存token
+    this.token = token
+    // 储存token
+    setToken(token)
     },
 
-    getInfo () {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          this.name = 'admin'
-          this.avatar = 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
-          this.menuRoutes = staticRoutes
-          resolve({name: this.name, avatar: this.avatar, token: this.token})
-        }, 1000)
-      })
+    // 获取用户信息的方法
+    async getInfo () {
+      const result = await infoApi()
+      // 储存用户信息
+      this.name = result.name       //用户名
+      this.avatar = result.avatar   //头像
+      // 储存按键和角色和路由的相关信息 
     },
 
-    reset () {
+    // 重置用户信息的方法
+    async reset () {
+      if (this.name) {
+        await logoutApi()
+      }
       // 删除local中保存的token
       removeToken()
       // 提交重置用户信息的mutation
--- src/utils/token-utils.ts
+++ src/utils/token-utils.ts
@@ -1,13 +1,14 @@
-const TokenKey = 'vue_admin_template_token'
-
+// 设置token的key
+const TokenKey = 'vue_admin_gshop'
+// 从缓存中读取token信息
 export function getToken() {
   return localStorage.getItem(TokenKey)
 }
-
+// 把token信息存储到缓存中
 export function setToken(token: string) {
   return localStorage.setItem(TokenKey, token)
 }
-
+// 移除token
 export function removeToken() {
   return localStorage.removeItem(TokenKey)
 }
