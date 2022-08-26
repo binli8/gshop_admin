b420ed8eb22675a12abaa568886ac310185fdf71
不如去早睡 <582181384@qq.com>
2022-08-26 18:27:21 +0800

    封装axios

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,9 +5,9 @@ import { useUserInfoStore } from '../stores/userInfo';
 
 /* 定义response对象的data接口 */
 interface ResponseData<T> {
-  code: number;
-  data: T;
-  message: string;
+	code: number;
+	data: T;
+	message: string;
 }
 
 // 配置新建一个 axios 实例
@@ -19,37 +19,44 @@ const service = axios.create({
 // 添加请求拦截器
 service.interceptors.request.use(
 	(config) => {
-    
+		// 获取用户信息仓库对象
+		const userInfoStore = useUserInfoStore()
+		// 获取token
+		const token = userInfoStore
+		// 判断token是否存在
+		if (token) {
+			(config.headers as any)['token'] = token
+		}
 		return config;
 	}
 );
 
 // 添加响应拦截器
 service.interceptors.response.use(
-  /* 约束一下response */
+	/* 约束一下response */
 	async (response: AxiosResponse<ResponseData<any>>) => {
 		// 对响应数据做点什么
 		const res = response.data;
-    if (res.code !== 20000 && res.code !== 200) { /* 成功数据的code值为20000/200 */
-      // 统一的错误提示
-      ElMessage({
-        message: (typeof res.data=='string' && res.data) || res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
+		if (res.code !== 20000 && res.code !== 200) { /* 成功数据的code值为20000/200 */
+			// 统一的错误提示
+			ElMessage({
+				message: (typeof res.data == 'string' && res.data) || res.message || 'Error',
+				type: 'error',
+				duration: 5 * 1000
+			})
 
 			// `token` 过期或者账号已在别处登录
-      if (response.status===401) {
-        const storeUserInfo = useUserInfoStore(pinia)
+			if (response.status === 401) {
+				const storeUserInfo = useUserInfoStore(pinia)
 				await storeUserInfo.reset()
 				window.location.href = '/' // 去登录页
 				ElMessageBox.alert('你已被登出，请重新登录', '提示', {})
-					.then(() => {})
-					.catch(() => {})
+					.then(() => { })
+					.catch(() => { })
 			}
 			return Promise.reject(service.interceptors.response);
 		} else {
-      return res.data; /* 返回成功响应数据中的data属性数据 */
+			return res.data; /* 返回成功响应数据中的data属性数据 */
 		}
 	},
 	(error) => {
