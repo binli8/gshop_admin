423157088c9399a921310aaa3cd66b97a472d5d2
不如去早睡 <582181384@qq.com>
2022-08-27 14:17:07 +0800

    分页器

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -7,7 +7,13 @@
       </div>
     </template>
     <!-- 表格 -->
-    <el-table :data="trademarkList" stripe border style="width: 100%">
+    <el-table
+      v-loading="loading"
+      :data="trademarkList"
+      stripe
+      border
+      style="width: 100%"
+    >
       <el-table-column type="index" label="序号" width="80" align="center" />
       <el-table-column prop="tmName" label="品牌名称" />
       <el-table-column label="品牌LOGO">
@@ -18,13 +24,23 @@
       <el-table-column prop="address" label="操作">
         <template v-slot="scope">
           <el-button size="small" type="warning" :icon="Edit"></el-button>
-          <el-button size="small" type="danger" :icon="Delete"
-            ></el-button
-          >
+          <el-button size="small" type="danger" :icon="Delete"></el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 分页 -->
+    <el-pagination
+      v-model:currentPage="current"
+      v-model:page-size="pageSize"
+      :page-sizes="[3,6,9]"
+      :small="small"
+      background
+      layout="  prev, pager, next, jumper,->,sizes,total"
+      :total="total"
+      @size-change="getTrademarkList(1,value)"
+      @current-change="getTrademarkList(value,pageSize)"
+      style="margin-top:20px"
+    />
     <!-- 对话框 -->
   </el-card>
 </template>
@@ -45,27 +61,36 @@ import {
 // 引入ref
 import { ref, onMounted } from "vue";
 // 引入品牌相关的接口函数
-import { getTrademarkListAllApi,getTrademarkListApi } from "@/api/product/trademark";
+import {
+  getTrademarkListAllApi,
+  getTrademarkListApi,
+} from "@/api/product/trademark";
 // 定义数组,用来收集品牌列表的数组数据信息
 const trademarkList = ref<TrademarkListModel>([]);
 const current = ref<number>(1); //页码数
 const pageSize = ref<number>(3); //每条的页数
 const total = ref<number>(0); //总页数
+// 定义加载标识
+const loading = ref<boolean>(false);
 // 定义一个函数,用来获取品牌列表的数据
 const getTrademarkList = async (
   page: number = current.value, //页码数
   limit: number = pageSize.value //每页显示的条数
 ) => {
-    // 更新页码和每页条数的数据
-    current.value = page
-    pageSize.value = limit
-    // 调用接口函数
+  // 更新页码和每页条数的数据
+  current.value = page;
+  pageSize.value = limit;
+  // 开启加载效果
+  loading.value = true;
+  // 调用接口函数
   const result = await getTrademarkListApi(page, limit);
-    //   更新数据
-    console.log(result);
-    
-    trademarkList.value = result.records
-    total.value = result.total
+  // 关闭加载效果
+  loading.value = false;
+  //   更新数据
+  console.log(result);
+
+  trademarkList.value = result.records;
+  total.value = result.total;
 };
 onMounted(() => {
   getTrademarkList();
