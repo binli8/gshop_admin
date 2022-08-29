1bdff2e8b7a190655d5d2f9c74c53075da53cf03
不如去早睡 <582181384@qq.com>
2022-08-29 22:35:44 +0800

    平台属性管理-增加修改静态完成

--- src/api/product/attr.ts
+++ src/api/product/attr.ts
@@ -16,7 +16,7 @@ enum Api {
     AddOrUpdateAttr = '/admin/product/saveAttrInfo'
 }
 // 定义接口函数
-// 根据三个分类的id获取对应的平台属性对象数组的接口函数
+// 根据三级分类的id获取对应的平台属性对象数组的接口函数
 export const getAttrInfoListApi = ({ category1Id, category2Id, category3Id }: CategoryIdModel) => request.get<any, AttrListModel>(Api.GetAttrInfoList + `/${category1Id}/${category2Id}/${category3Id}`)
 // 根据id删除指定的平台属性对象的接口函数
 export const deleteAttrByIdApi = (attrId: number) => request.delete<any, null>(Api.DeleteAttrById + `/${attrId}`)
