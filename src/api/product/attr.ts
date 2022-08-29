3c55b39dfd26e7589e2393b32db18805e20d2d89
不如去早睡 <582181384@qq.com>
2022-08-29 16:39:06 +0800

    平台属性管理接口函数定义完成

---
+++ src/api/product/attr.ts
@@ -0,0 +1,33 @@
+// 引入request
+import request from '@/utils/request'
+// 引入(平台属性值对象接口类型,平台属性值对象数组接口类型,平台属性对象接口类型,平台属性值对象数组接口类型)
+import type { AttrValueModel, AttrValueListModel, AttrModel, AttrListModel } from './model/attrModel'
+// 引入带有三个分类id的接口类型
+import type { CategoryIdModel } from './model/categoryModel'
+// 定义枚举接口地址
+enum Api {
+    // 根据三级分类获取平台属性数据的地址
+    GetAttrInfoList = '/admin/product/attrInfoList/{category1Id}/',
+    // 根据删除id平台属性的接口地址
+    DeleteAttr = '/admin/product/deleteAttr/',
+    // 根据id获取平台属性值对象数组的接口地址
+    GetAttrValueList = '/admin/product/getAttrValueList/',
+    // 添加或者修改平台属性的接口地址
+    SaveAttrInfo = '/admin/product/saveAttrInfo'
+}
+// 根据三个分类的id获取对应的平台属性对象数组的接口函数
+export const getAttrInfoListApi = ({ category1Id, category2Id, category3Id }: CategoryIdModel) => request.get<any, AttrListModel>(Api.GetAttrInfoList + `${category1Id}/${category2Id}/${category3Id}`)
+// 根据id删除指定指定的平台属性对象的接口函数
+export const deleteAttrApi = (attrId: number) => request.delete<any, null>(Api.DeleteAttr + `${attrId}`)
+// 根据id获取对应的平台属性值对象的数组的接口函数
+export const GetAttrValueList = (attrId: number) => request.get<any, AttrValueListModel>(Api.GetAttrValueList + `${attrId}`)
+// 添加或者修改平台属性对象的接口函数
+export const GetAttrInfoList = (arrt: AttrModel) => request.post<any, null>(Api.GetAttrInfoList, arrt)
+
+
+// GET 
+// getAttrValueList
+
+// POST 
+// 
+
--- src/api/product/model/categoryModel.ts
+++ src/api/product/model/categoryModel.ts
@@ -1,9 +1,18 @@
 //  定义三级分类对象接口的类型
-export interface CategoryModel{
-    id?:number
-    name:string
-    categoryId?:number          //所属的分类的id
-    hasChildren?:boolean        //当前的分类中是否还有其他的子级分类
+export interface CategoryModel {
+    id?: number
+    name: string
+    categoryId?: number          //所属的分类的id
+    hasChildren?: boolean        //当前的分类中是否还有其他的子级分类
 }
 // 定义三级分类对象数组的接口类型
 export type CategoryListModel = CategoryModel[]
+
+// 定义分类id的类型
+export type NumberAble = number | undefined
+// 定义带有三个分类id的接口类型
+export interface CategoryIdModel {
+    category1Id: NumberAble
+    category2Id: NumberAble
+    category3Id: NumberAble
+}
