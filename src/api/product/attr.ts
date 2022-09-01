b84f7921b9c75a094aa5b5746e5d96bd4787864a
不如去早睡 <582181384@qq.com>
2022-09-01 17:01:46 +0800

    更新

--- src/api/product/attr.ts
+++ src/api/product/attr.ts
@@ -1,20 +1,26 @@
-// 引入request
+// 引入 request
 import request from '@/utils/request'
-// 引入平台属性相关数据的接口类型
-import type { AttrModel, AttrListModel, AttrValueListModel } from './model/attrModel'
+// 引入平台属性相关数据的接口类型(平台属性值对象接口类型,属性值对象数组接口类型,平台属性对象接口类型,平台属性对象数组接口类型)
+import type { AttrValueModel, AttrValueListModel, AttrModel, AttrListModel ,} from './model/attrModel'
+//引入带有三个分类id的接口类型
 import type { CategoryIdModel } from './model/categoryModel'
 // 定义枚举类型的接口地址
 enum Api {
-    GetAttrInfoList = '/admin/product/attrInfoList', // 根据三级分类获取平台属性数据的地址
-    DeleteAttrById = '/admin/product/deleteAttr', // 根据id删除平台属性的接口地址
-    GetAttrValueListById = '/admin/product/getAttrValueList', // 根据id获取平台属性值对象数组的接口地址
-    AddOrUpdateAttr = '/admin/product/saveAttrInfo' // 添加或者修改平台属性的接口地址
+    // 根据三级分类获取平台属性数据的地址
+    GetAttrInfoList = '/admin/product/attrInfoList',
+    // 根据id删除平台属性的接口地址
+    DeleteAttrById = '/admin/product/deleteAttr',
+    // 根据id获取平台属性值对象数组的接口地址
+    GetattrValueListByid = '/admin/product/getAttrValueList',
+    // 添加或者修改平台属性的接口地址
+    AddOrUpdateAttr = '/admin/product/saveAttrInfo'
 }
-// 根据三个分类的id获取对应的平台属性对象数组的接口函数
-export const getAttrInfoListApi = ({ category1Id, category2Id, category3Id }: CategoryIdModel) => request.get<any, AttrListModel>(Api.GetAttrInfoList + `/${category1Id}/${category2Id}/${category3Id}`)
+// 定义接口函数
+// 根据三级分类的id获取对应的平台属性对象数组的接口函数
+export const getAttrInfoListApi = ({ category1Id, category2Id, category3Id }: CategoryIdModel) => request.get<any,AttrListModel>(Api.GetAttrInfoList +`/${category1Id}/${category2Id}/${category3Id}`)
 // 根据id删除指定的平台属性对象的接口函数
 export const deleteAttrByIdApi = (attrId: number) => request.delete<any, null>(Api.DeleteAttrById + `/${attrId}`)
 // 根据id获取对应的平台属性值对象的数组的接口函数
-export const getAttrValueListByIdApi = (attrId: number) => request.get<any, AttrValueListModel>(Api.GetAttrValueListById + `/${attrId}`)
+export const getattrValueListByidApi = (attrId: number) => request.get<any, AttrValueListModel>(Api.GetattrValueListByid + `/${attrId}`)
 // 添加或者修改平台属性对象的接口函数
 export const addOrUpdateAttrApi = (attr: AttrModel) => request.post<any, null>(Api.AddOrUpdateAttr, attr)
---
+++ src/api/product/sku.ts
@@ -0,0 +1,51 @@
+// 引入request
+import request from '@/utils/request'
+// 引入 接口类型
+import type { SkuInfoModel, SkuInfoListModel, SkuPageListParamsModel } from './model/skuModel'
+// 定义枚举
+enum Api {
+    // 上架的接口地址
+    OnSale = '/admin/product/onSale',
+    // 下架的接口地址
+    CancelSale = '/admin/product/cancelSale',
+    // 根据id删除sku对象的接口地址
+    DeleteSkuInfoById = '/admin/product/deleteSku',
+    // 根据spu的id获取spu对象的数组列表数据的接口
+    GetSkuInfoListBySpuId = '/admin/product/findBySpuId',
+    // 根据sku的id获取对应的sku对象的接口地址
+    GetSkuInfoById = '/admin/product/getSkuInfo',
+    // 根据关键字获取sku对象数组列表数据的接口地址
+    GetSkuInfoListByKeyword = '/admin/product/inner/findSkuInfoByKeyword',
+    // 根据id数组获取sku对象数组列表数据的接口地址
+    GetSkuInfoListBySkuIds = '/admin/product/inner/findSkuInfoBySkuIdList',
+    // 分页的方式获取sku列表数据的接口地址
+    GetSkuInfoList = '/admin/product/list',
+    // 添加sku对象的接口地址
+    AddSkuInfo = '/admin/product/saveSkuInfo',
+    // 修改sku对象的接口地址
+    UpdateSkuInfo = '/admin/product/updateSkuInfo'
+}
+
+// 定义接口函数
+// 上架的接口函数
+export const onSaleApi = (skuId: number) => request.get<any, null>(Api.OnSale + `/${skuId}`)
+// 下架的接口函数
+export const cancelSaleApi = (skuId: number) => request.get<any, null>(Api.CancelSale + `/${skuId}`)
+// 根据id删除sku对象的接口函数
+export const deleteSkuInfoByIdApi = (skuId: number) => request.delete<any, null>(Api.DeleteSkuInfoById + `/${skuId}`)
+// 根据spu的id获取spu对象的数组列表数据的函数
+export const getSkuInfoListBySpuIdApi = (skuId: number) => request.get<any, SkuInfoListModel>(Api.GetSkuInfoListBySpuId + `/${skuId}`)
+// 根据sku的id获取对应的sku对象的接口函数
+export const getSkuInfoByIdApi = (skuId: number) => request.get<any, SkuInfoModel>(Api.GetSkuInfoById + `/${skuId}`)
+// 根据关键字获取sku对象数组列表数据的接口函数
+export const getSkuInfoListByKeywordApi = (keyword: string) => request.get<any, SkuInfoListModel>(Api.GetSkuInfoListByKeyword + `/${keyword}`)
+// 根据id数组获取sku对象数组列表数据的接口函数
+export const getSkuInfoListBySkuIdsApi = (ids: number[]) => request.post<any, null>(Api.GetSkuInfoListBySkuIds, ids)
+// 分页的方式获取sku列表数据的接口函数
+export const getSkuInfoListApi = (page: number, limit: number) => request.get<any, SkuPageListParamsModel>(Api.GetSkuInfoList + `/${page}/${limit}`)
+// 添加sku对象的接口函数
+export const addSkuInfoApi = (skuInfo: SkuInfoModel) => request.post<any, null>(Api.AddSkuInfo, skuInfo)
+// 修改sku对象的接口函数
+export const updateSkuInfoApi = (skuInfo: SkuInfoModel) => request.post<any, null>(Api.UpdateSkuInfo, skuInfo)
+// 修改或添加sku数据的接口函数
+export const addOrUpdteSkuInfoApi = (skuInfo: SkuInfoModel) => skuInfo.id ? updateSkuInfoApi(skuInfo) : addSkuInfoApi(skuInfo)
